393ca286ca7840ecc15d7d1ff4906bafd59fc66e
Daniel Borcherding <danielborcherding@DanielBdingsMBP.home>
2020-11-17 22:30:04 +0100

    changed homepage

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//"homepage": "http://danielsdatahacks.github.io/notas",
