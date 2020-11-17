ca2a144b80874e67ba0faba0921e0994b26d5f95
Daniel Borcherding <danielborcherding@DanielBdingsMBP.home>
2020-11-17 21:48:19 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./web.config";
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
