84e919fb1a14b30ae20fef3a547032c17807f73c
Daniel Borcherding <danielborcherding@DanielBdingsMBP.home>
2020-11-21 23:57:14 +0100

    azure function with db connection

--- api/LoadNotasGraph.cs
+++ api/LoadNotasGraph.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Test;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Notas.Function
 {
@@ -17,47 +21,33 @@ namespace Notas.Function
         [FunctionName("LoadNotasGraph")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, ExecutionContext context)
         {
-            // log.LogInformation("C# HTTP trigger function processed a request.");
-
-            // string name = req.Query["name"];
-
-            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // name = name ?? data?.name;
-
-            // string responseMessage = string.IsNullOrEmpty(name)
-            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            // return new OkObjectResult(responseMessage);
-
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var products = new List<Product>
-            {
-                new Product
-                {
-                    Name = "Phone",
-                    Price = 999.90m,
-                    Description = "This is the description of the phone"
-                },
-                new Product
-                {
-                    Name = "Book",
-                    Price = 99.90m,
-                    Description = "The best book you will ever read"
-                },
-                new Product
-                {
-                    Name = "TV",
-                    Price = 15.49m,
-                    Description = "Here you can see an awesome TV"
-                }
-            };
-    
-            return new OkObjectResult(JsonConvert.SerializeObject(products));
+            var SQL = @"SELECT  [PersonID],
+                                [LastName],
+                                [FirstName],
+                                [Address],
+                                [City]
+                        FROM [dbo].[Persons]";
+
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(context.FunctionAppDirectory)
+                            .AddJsonFile("local.settings.json", optional:true, reloadOnChange: true)
+                            .AddEnvironmentVariables()
+                            .Build();
+
+            string appsettingvalue = config["ConnectionString"];
+
+            List<Customer> customers = new List<Customer>();
+            using (IDbConnection conn = new SqlConnection(appsettingvalue)){
+
+                customers = conn.Query<Customer>(SQL).AsList();
+            
+            }
+
+            return new OkObjectResult(JsonConvert.SerializeObject(customers));
         }
     }
 }
---
+++ api/models/Customer.cs
@@ -0,0 +1,15 @@
+namespace Test
+{
+    public class Customer
+    {
+        public int PersonID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+    }
+}
